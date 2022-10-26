b0ee50a15e3d569f5aa2e2138e86e4bb21e69393
Klaas Bosteels <klaas.bosteels@gmail.com>
2022-10-26 13:20:45 +0200

    Release 0.7

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ extras_require = {
 
 setup(
     name="refill",
-    version="0.6",
+    version="0.7",
     description="Reimagined filling of document templates",
     license="Apache License, Version 2.0",
     url="https://github.com/trackuity/refill",
